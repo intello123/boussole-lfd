d14076ea0f511829dc2946f0df284feccd879cc7
intello123 <93282495+intello123@users.noreply.github.com>
2021-10-27 14:22:03 -0400

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,11 +4,11 @@ basic.forever(function () {
     if (degres < 45) {
         basic.showString("N")
     } else if (degres < 135) {
-        basic.showString("e")
+        basic.showString("E")
     } else if (degres < 225) {
-        basic.showString("s")
+        basic.showString("S")
     } else if (degres < 315) {
-        basic.showString("w")
+        basic.showString("W")
     } else {
         basic.showString("N")
     }
